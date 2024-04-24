8bbf867a82c8a64f8325c7ffd858ceea90c4a1d4
LeQuyetTien1 <lqtien1404@gmail.com>
2024-04-24 22:34:56 +0700

    edit24.4

--- My 3D project/Assets/Scripts/DragDrop.cs
+++ My 3D project/Assets/Scripts/DragDrop.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 public class DragDrop : MonoBehaviour
 {
-    Vector3 offset;
-    /*private void OnMouseDown()
-    {
-        offset = transform.position - MouseWorldPosition();
-    }*/
+    public LayerMask layerMask;
     private void OnMouseDrag()
     {
         transform.position = new Vector3(MouseWorldPosition().x,0.75f,MouseWorldPosition().z);
+        transform.eulerAngles = Vector3.zero;
+        gameObject.GetComponent<Rigidbody>().isKinematic = false;
     }
     private Vector3 MouseWorldPosition()
     {
@@ -22,9 +20,19 @@ public class DragDrop : MonoBehaviour
     private void OnMouseUp()
     {
         RaycastHit hit = CastRay();
-        if (hit.collider.CompareTag("Plane"))
+        
+        if (hit.collider.CompareTag("Plane1"))
         {
-            transform.position = hit.transform.position;
+            transform.position = new Vector3(-1.09f, 0.21f, -3.8f); ;
+            transform.eulerAngles = new Vector3(-90, 0, 0);
+            gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            Debug.Log("Plane");
+        }
+        else if (hit.collider.CompareTag("Plane2"))
+        {
+            transform.position = new Vector3(0.91f, 0.21f, -3.8f); ;
+            transform.eulerAngles = new Vector3(-90, 0, 0);
+            gameObject.GetComponent<Rigidbody>().isKinematic = true;
             Debug.Log("Plane");
         }
     }
@@ -32,8 +40,10 @@ public class DragDrop : MonoBehaviour
     {
         Vector3 screenMousePosFar = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.farClipPlane);
         Vector3 worldMousePosFar = Camera.main.ScreenToWorldPoint(screenMousePosFar);
+        Vector3 screenMousePosNear = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane);
+        Vector3 worldMousePosNear = Camera.main.ScreenToWorldPoint(screenMousePosNear);
         RaycastHit hit;
-        Physics.Raycast(transform.position, worldMousePosFar - transform.position, out hit);
+        Physics.Raycast(transform.position, worldMousePosFar - transform.position, out hit, layerMask);
         return hit;
     }
 }
---
+++ My 3D project/Assets/Scripts/Slot.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slot : MonoBehaviour
+{
+    public bool isOccupied = false;
+    private void Update()
+    {
+        bool isHit = CastRay();
+        if (isHit == true)
+        {
+            isOccupied = true;
+        }
+        else
+        {
+            isOccupied = false;
+        }
+    }
+    private bool CastRay()
+    {
+        Vector3 screenMousePosFar = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.farClipPlane);
+        Vector3 worldMousePosFar = Camera.main.ScreenToWorldPoint(screenMousePosFar);
+        Vector3 screenMousePosNear = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane);
+        Vector3 worldMousePosNear = Camera.main.ScreenToWorldPoint(screenMousePosNear);
+        return Physics.Raycast(transform.position, worldMousePosNear - transform.position);
+    }
+}
