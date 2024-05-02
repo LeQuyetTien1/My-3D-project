fa32c6dd6b4fd9a8a6cbf0e93ae206aa13fba27d
LeQuyetTien1 <lqtien1404@gmail.com>
2024-05-02 20:46:13 +0700

    fix2.5

--- My 3D project/Assets/Scripts/DragDrop.cs
+++ My 3D project/Assets/Scripts/DragDrop.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DragDrop : MonoBehaviour
 {
     public LayerMask layerMask;
+    private Vector3 slot1Pos = new Vector3(-1.09f, 0.21f, -3.8f);
+    private Vector3 slot2Pos = new Vector3(0.91f, 0.21f, -3.8f);
     private void OnMouseDrag()
     {
         transform.position = new Vector3(MouseWorldPosition().x,0.75f,MouseWorldPosition().z);
         transform.eulerAngles = Vector3.zero;
+        gameObject.GetComponent<Rigidbody>().useGravity = false;
         gameObject.GetComponent<Rigidbody>().isKinematic = false;
     }
     private Vector3 MouseWorldPosition()
@@ -20,20 +24,15 @@ public class DragDrop : MonoBehaviour
     private void OnMouseUp()
     {
         RaycastHit hit = CastRay();
-        
+        gameObject.GetComponent<Rigidbody>().useGravity = true;
+
         if (hit.collider.CompareTag("Plane1"))
         {
-            transform.position = new Vector3(-1.09f, 0.21f, -3.8f); ;
-            transform.eulerAngles = new Vector3(-90, 0, 0);
-            gameObject.GetComponent<Rigidbody>().isKinematic = true;
-            Debug.Log("Plane");
+            FitSlot(slot1Pos);
         }
         else if (hit.collider.CompareTag("Plane2"))
         {
-            transform.position = new Vector3(0.91f, 0.21f, -3.8f); ;
-            transform.eulerAngles = new Vector3(-90, 0, 0);
-            gameObject.GetComponent<Rigidbody>().isKinematic = true;
-            Debug.Log("Plane");
+            FitSlot(slot2Pos);
         }
     }
     private RaycastHit CastRay()
@@ -45,5 +44,11 @@ public class DragDrop : MonoBehaviour
         RaycastHit hit;
         Physics.Raycast(transform.position, worldMousePosFar - transform.position, out hit, layerMask);
         return hit;
+    }
+    private void FitSlot(Vector3 slotPos)
+    {
+        transform.position = slotPos;
+        transform.eulerAngles = new Vector3(-90, 0, 0);
+        gameObject.GetComponent<Rigidbody>().isKinematic = true;
     }
 }
--- My 3D project/Assets/Scripts/Slot.cs
+++ My 3D project/Assets/Scripts/Slot.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Slot : MonoBehaviour
 {
     public bool isOccupied = false;
-    private void Update()
+    /*private void Update()
     {
         bool isHit = CastRay();
         if (isHit == true)
@@ -24,5 +24,17 @@ public class Slot : MonoBehaviour
         Vector3 screenMousePosNear = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane);
         Vector3 worldMousePosNear = Camera.main.ScreenToWorldPoint(screenMousePosNear);
         return Physics.Raycast(transform.position, worldMousePosNear - transform.position);
+    }*/
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOccupied = true;
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        isOccupied = true;
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        isOccupied = false;
     }
 }
